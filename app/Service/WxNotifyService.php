68b9a7070685cc78902f1071f7321399531380a4
李克勤 <269868211@qq.com>
2017-12-24 03:28:47 +0800

    上线

--- app/Service/WxNotifyService.php
+++ app/Service/WxNotifyService.php
@@ -103,13 +103,13 @@ class WxNotifyService extends WxPayNotify
         foreach ($cardInfo as $key => $value) {
 
             $userCards = new UserCards();
-            $userCards->user_id = $uid;
-            $userCards->card_id = 1;
-            $userCards->card_code = mt_rand(10000000, 99999999);   // 生成
+            $userCards->user_id = 1;
+            $userCards->card_id = $value->id;
+            $userCards->card_code = $this->getCardCode();   // 生成
             $userCards->card_code_pw = mt_rand(10000000, 99999999);    // 随机生成八位数字
-            $userCards->number = 1;
-            $userCards->number_count = 1;
-            $userCards->number_last = 1;
+            $userCards->number = $value->number * $pNumber[$key];
+            $userCards->number_count = $value->number * $pNumber[$key];
+            $userCards->number_last = $value->number * $pNumber[$key];
             $userCards->card_source = 1;
             $userCards->address = '1';
 
@@ -122,7 +122,7 @@ class WxNotifyService extends WxPayNotify
     public function getCardCode()
     {
         // 随机生成数字
-        $code = '89' + mt_rand(1000000, 9999999);
+        $code = '89'+ mt_rand(1000000, 9999999);
         // 查询卡号是否存在
         $cardInfo = UserCards::where('card_code', $code)->count();
         if ($cardInfo > 0) {
